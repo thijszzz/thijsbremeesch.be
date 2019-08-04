9459178c8ba305a23bf8ec9ef87fdc06a2916521
Wouter Landuydt <wouter.landuydt@gmail.com>
2019-08-04 12:33:46 +0200

    added more data on project page

--- src/templates/project.js
+++ src/templates/project.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import SEO from "../components/seo"
 import PropTypes from "prop-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 const ProjectPage = ({ data }) => {
   const { contentfulProject: project } = data
@@ -10,7 +11,37 @@ const ProjectPage = ({ data }) => {
   return (
     <Layout>
       <SEO title={project.title} />
-      {JSON.stringify(project)}
+      <h1>{project.title}</h1>
+      <dl>
+        <img src={project.headerImage.fixed.src} alt=""></img>
+        <dt>Client</dt>
+        <dd>
+          {project.clients.map(({ name }) => (
+            <span key={name}>{name}</span>
+          ))}
+        </dd>
+
+        <dt>Category</dt>
+        <dd>{project.category}</dd>
+
+        <dt>Date</dt>
+        <dd>{project.date}</dd>
+
+        <dt>Team</dt>
+        <dd>
+          {project.team.map(name => (
+            <span key={name}>{name}</span>
+          ))}
+        </dd>
+
+        <dt>Link</dt>
+        <dd>
+          <a href={project.link}>{project.link}</a>
+        </dd>
+      </dl>
+
+      <p>{project.intro.intro}</p>
+      <div>{documentToReactComponents(project.body.json)}</div>
     </Layout>
   )
 }
@@ -23,8 +54,27 @@ export const pageQuery = graphql`
   query($slug: String!) {
     contentfulProject(slug: { eq: $slug }) {
       slug
-      title
+      category
       date
+      headerImage {
+        fixed {
+          src
+        }
+      }
+      id
+      body {
+        json
+      }
+      clients {
+        name
+        link
+      }
+      intro {
+        intro
+      }
+      link
+      team
+      title
     }
   }
 `
