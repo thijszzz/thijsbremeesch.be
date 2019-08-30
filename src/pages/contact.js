7619dd04aa09a6299a664f46a333f656125f4e6b
Wouter Landuydt <wouter.landuydt@gmail.com>
2019-08-30 23:25:52 +0200

    change name for stupid git

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Layout } from "../components/layout"
+import { Layout } from "../components/Layout"
 import { navigate } from "gatsby-link"
 import SEO from "../components/seo"
 import { encode } from "../lib"
--- src/templates/project.js
+++ src/templates/project.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Layout } from "../components/Layout/Layout"
+import { Layout } from "../components/Layout"
 import { graphql, Link } from "gatsby"
 import SEO from "../components/seo"
 import PropTypes from "prop-types"
