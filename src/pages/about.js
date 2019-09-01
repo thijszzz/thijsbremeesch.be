18bc0996a14f9e0532427f109a61220b7427eec8
Wouter Landuydt <wouter.landuydt@gmail.com>
2019-09-01 13:08:45 +0200

    about page setup & basic style

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,77 @@
 import React from "react"
 import { Layout } from "../components/Layout"
 import SEO from "../components/seo"
+import styles from "./about.module.css"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import Img from "gatsby-image"
+import HiOutline from "../assets/svg/hi_outline"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 const AboutPage = () => {
+  const data = useStaticQuery(query)
+  const { contentfulAboutPage } = data
+
   return (
     <Layout>
       <SEO title="About" />
-      <p>Hi it's me</p>
+
+      <section className={styles.mainContainer}>
+        <Img
+          sizes={contentfulAboutPage.image.sizes}
+          className={styles.mainImage}
+        />
+
+        <div className={styles.mainContentContainer}>
+          <HiOutline />
+          <div className={styles.mainIntroContainer}>
+            {documentToReactComponents(contentfulAboutPage.intro.json)}
+          </div>
+          <ol className={styles.mainLinkList}>
+            {contentfulAboutPage.resume && (
+              <li>
+                <a href={contentfulAboutPage.resume.file.url}>
+                  Check out my resume
+                </a>
+              </li>
+            )}
+            <li>
+              <Link to="/contact">Contact me</Link>
+            </li>
+          </ol>
+        </div>
+      </section>
+
+      <span>{contentfulAboutPage.quote}</span>
+
+      <section>
+        {documentToReactComponents(contentfulAboutPage.body.json)}
+      </section>
     </Layout>
   )
 }
+
+const query = graphql`
+  query {
+    contentfulAboutPage(slug: { eq: "about-page" }) {
+      body {
+        json
+      }
+      image {
+        sizes(maxWidth: 460) {
+          ...GatsbyContentfulSizes_withWebp
+        }
+      }
+      quote
+      intro {
+        json
+      }
+      resume {
+        file {
+          url
+        }
+      }
+    }
+  }
+`
 
 export default AboutPage
