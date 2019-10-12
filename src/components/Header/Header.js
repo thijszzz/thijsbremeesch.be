f29fb47c395ca977cf809e4aa7a7fe53c1e99988
Wouter Landuydt <wouter.landuydt@gmail.com>
2019-10-12 11:17:38 +0200

    wip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,27 +3,50 @@ import PropTypes from "prop-types"
 import React from "react"
 import styles from "./header.module.css"
 import SignatureLogo from "../../assets/svg/signature_logo"
+import lottie from "lottie-web"
+import animationData from "../../assets/lottie/logo_large.json"
 
-export const Header = ({ siteTitle }) => (
-  <header className={styles.header}>
-    <h1>
-      <span className="hide">{siteTitle} </span>
-      <Link to="/">
-        <SignatureLogo />
-      </Link>
-    </h1>
+export const Header = ({ siteTitle }) => {
+  const logoRef = React.useRef(null)
 
-    <nav className={styles.nav}>
-      <ol className={styles.navList}>
-        <li className={styles.navListItem}>
-          <Link className={styles.navListLink} to="/about">
-            about
+  React.useEffect(() => {
+    console.log(logoRef.current, animationData)
+    lottie.loadAnimation({
+      container: logoRef.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      // path: "../../assets/lottie/logo_large",
+      animationData,
+    })
+
+    return () => lottie.destroy()
+  }, [logoRef])
+
+  return (
+    <>
+      <div ref={logoRef}>Thijs</div>
+      <header className={styles.header}>
+        <h1>
+          <span className="hide">{siteTitle} </span>
+          <Link to="/">
+            <SignatureLogo />
           </Link>
-        </li>
-      </ol>
-    </nav>
-  </header>
-)
+        </h1>
+
+        <nav className={styles.nav}>
+          <ol className={styles.navList}>
+            <li className={styles.navListItem}>
+              <Link className={styles.navListLink} to="/about">
+                about
+              </Link>
+            </li>
+          </ol>
+        </nav>
+      </header>
+    </>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
